f662d13abed68e4fb1e002eff5e455944ac6c3a6
QuentinLp <quentin.le-port@ensg.eu>
2021-06-17 16:20:16 +0200

    Test data tree structure creation : deal with comment order and depth

---
+++ tests/test_data_tree_structure.py
@@ -0,0 +1,67 @@
+"""
+Test data structure
+"""
+import pytest
+from mapping.data_manipulation import init_index, get_data
+from mapping.utils.node_proposal import NodeProposal
+from mapping.utils.node_comment import NodeComment
+
+df_prop_config1, df_coms_config1 = get_data("./../test_data/extracts/comments_config1.xls",
+                                            "./../test_data/extracts/proposals_config1.xls")
+df_prop_config2, df_coms_config2 = get_data("./../test_data/extracts/comments_config2.xls",
+                                            "./../test_data/extracts/proposals_config2.xls")
+df_prop_config3, df_coms_config3 = get_data("./../test_data/extracts/comments_config3.xls",
+                                            "./../test_data/extracts/proposals_config3.xls")
+
+
+def nodes_equal(node1, node2):
+    is_equal = node1.body == node2.body
+    for childNode1, childNode2 in zip(node1.children, node2.children):
+        is_equal = is_equal and nodes_equal(childNode1, childNode2)
+    return is_equal
+
+
+CONFIG_1 = [NodeProposal(title="P0 titre",
+                         body="P0 body",
+                         supports=12,
+                         children=[NodeComment(body="C1",
+                                               children=[NodeComment(body="C0",
+                                                                     children=[NodeComment(body="R1",
+                                                                                           children=[])]),
+                                                         NodeComment(body="R2", children=[])])])]
+
+CONFIG_2 = [NodeProposal(title="P0 titre",
+                         body="P0",
+                         supports=12,
+                         children=[NodeComment(body='C0', children=[]),
+                                   NodeComment(body='C1', children=[]),
+                                   NodeComment(body='C2', children=[]),
+                                   NodeComment(body='C3', children=[])]),
+            NodeProposal(title="P1 titre",
+                         body="P1",
+                         supports=5,
+                         children=[])]
+
+CONFIG_3 = [NodeProposal(title="P0 titre",
+                         body="P0",
+                         supports=12,
+                         children=[NodeComment(body='C1',
+                                               children=[NodeComment(body="R1",
+                                                                     children=[NodeComment(body="R2",
+                                                                                           children=[NodeComment(body="R3", children=[])])])]),
+                                   NodeComment(body='C2', children=[NodeComment(body="R4", children=[])])]),
+            NodeProposal(title="P1 titre",
+                         body="P1",
+                         supports=5,
+                         children=[])]
+
+TEST_CASES = [(df_prop_config1, df_coms_config1, CONFIG_1),
+              (df_prop_config2, df_coms_config2, CONFIG_2),
+              (df_prop_config3, df_coms_config3, CONFIG_3)]
+
+
+@pytest.mark.parametrize("proposal_dataframe, comments_dataframe, output ", TEST_CASES)
+def test_tree_structure(proposal_dataframe, comments_dataframe, output):
+    nodes = init_index(proposal_dataframe, comments_dataframe)
+    for node_test, node_validation in zip(nodes, output):
+        assert nodes_equal(node_test, node_validation)

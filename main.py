1b5dd1fef14f884c62e6545a42cca78be798f11d
Armand Fardeau <fardeauarmand@gmail.com>
2021-07-07 15:37:19 +0200

    Use proper file

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from mapping.data_manipulation import init_index, init_txt, init_csv, init_docx,
 
 if __name__ == '__main__':
     df_proposals, df_comments = get_data(os.path.join(os.getcwd(),
-                                                      "test_data/comments_FEAMP.xls"),
+                                                      "test_data/comments_config1.xls"),
                                          os.path.join(os.getcwd(),
-                                                      "test_data/proposals_FEAMP.xls"))
+                                                      "test_data/proposals_config1.xls"))
     hsh_prop = init_index(proposals_dataframe=df_proposals, comments_dataframe=df_comments)
     init_csv(hsh_prop)
     init_txt(hsh_prop, sorting_attribute="supports")

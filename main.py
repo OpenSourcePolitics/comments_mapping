66851caa06c528d4cb1c5480d51c9f5ba52d817f
QuentinLp <quentin.le-port@ensg.eu>
2021-07-08 09:42:36 +0200

    Change os.getcwd() by absolute path

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 Execution of data_manipulation.py
 """
 import os
+import re
 from mapping.data_manipulation import init_index, init_txt, init_csv, init_docx, get_data
 
 if __name__ == '__main__':
-    df_proposals, df_comments = get_data(os.path.join(os.getcwd(),
-                                                      "test_data/comments_config1.xls"),
-                                         os.path.join(os.getcwd(),
-                                                      "test_data/proposals_config1.xls"))
+    main_path = os.path.realpath(__file__)
+    root_path, _ = os.path.split(os.path.split(main_path)[0])
+    df_proposals, df_comments = get_data(os.path.join(root_path,
+                                                      "comments_mapping/test_data/comments_config1.xls"),
+                                         os.path.join(root_path,
+                                                      "comments_mapping/test_data/proposals_config1.xls"))
     hsh_prop = init_index(proposals_dataframe=df_proposals, comments_dataframe=df_comments)
     init_csv(hsh_prop)
     init_txt(hsh_prop, sorting_attribute="supports")

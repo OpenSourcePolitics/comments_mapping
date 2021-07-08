a7267a8496ecac0f0d10296ee23c0ec303e78efd
QuentinLp <quentin.le-port@ensg.eu>
2021-07-08 09:50:30 +0200

    Fixing absolute path call in main (adding comments_mapping in root path rather than manual add)

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from mapping.data_manipulation import init_index, init_txt, init_csv, init_docx,
 
 if __name__ == '__main__':
     main_path = os.path.realpath(__file__)
-    root_path, _ = os.path.split(os.path.split(main_path)[0])
+    root_path = os.path.split(main_path)[0]
     df_proposals, df_comments = get_data(os.path.join(root_path,
-                                                      "comments_mapping/test_data/comments_config1.xls"),
+                                                      "test_data/comments_config1.xls"),
                                          os.path.join(root_path,
-                                                      "comments_mapping/test_data/proposals_config1.xls"))
+                                                      "test_data/proposals_config1.xls"))
     hsh_prop = init_index(proposals_dataframe=df_proposals, comments_dataframe=df_comments)
     init_csv(hsh_prop)
     init_txt(hsh_prop, sorting_attribute="supports")

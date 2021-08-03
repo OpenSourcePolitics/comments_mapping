b5b61179dbe7b68b57cc92974fb9e467e1396f64
QuentinLp <quentin.le-port@ensg.eu>
2021-08-03 17:52:17 +0200

    Refactoring main execution : changing inputs from csv, xls to json

--- main.py
+++ main.py
@@ -4,16 +4,10 @@ Execution of data_manipulation.py
 import os
 from mapping.data_manipulation import init_index, init_txt, init_csv, init_docx, get_data
 
-def main(sorting_attribute=None):
-    main_path = os.path.realpath(__file__)
-    root_path = os.path.split(main_path)[0]
-    df_proposals, df_comments = get_data(os.path.join(root_path,
-                                                      "test_data/comments_config1.xls"),
-                                         os.path.join(root_path,
-                                                      "test_data/proposals_config1.xls"))
+
+def main(json_object, sorting_attribute=None):
+    df_proposals, df_comments = get_data(json_object)
     hsh_prop = init_index(proposals_dataframe=df_proposals, comments_dataframe=df_comments)
     init_csv(hsh_prop)
-    init_txt(hsh_prop, sorting_attribute="supports")
-    init_docx(hsh_prop, sorting_attribute="supports")
-
-main()
+    init_txt(hsh_prop, sorting_attribute=sorting_attribute)
+    init_docx(hsh_prop, sorting_attribute=sorting_attribute)

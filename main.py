ffe729beb643f12152df97c0523b9e8e06818ebd
QuentinLp <quentin.le-port@ensg.eu>
2021-08-04 14:11:43 +0200

    Documentation and update get_data parameters to be able to read local data

--- main.py
+++ main.py
@@ -22,14 +22,19 @@ def prepare_archive():
             new_zip.write(os.path.relpath(os.path.join(dir_path, file)))
 
 
-def map_comments_with_proposals(json_object, sorting_attribute=None):
+def map_comments_with_proposals(post_request_result=None, local_json_data_path=None, sorting_attribute=None):
     """
-    Main execution function : initialize all resources
-    :param json_object:
-    :param sorting_attribute:
-    :return:
+    Main execution function : initialize all the outputs and generate an archive (.zip) containing
+    the three generated formats
+    :param post_request_result: dictionary containing the data transmitted to the API with a request POST.
+    :type post_request_result: dict
+    :param local_json_data_path: path to the json file saved locally that contains the data.
+    :type local_json_data_path: str
+    :param sorting_attribute: parameter passed either with the POST request or manually to indicates how the data
+    must be sorted in the output either by number of comments or by number of supports.
+    :type sorting_attribute: str
     """
-    df_proposals, df_comments = get_data(json_object)
+    df_proposals, df_comments = get_data(post_request_json_object=post_request_result, local_json_file_path=local_json_data_path)
     hsh_prop = init_index(proposals_dataframe=df_proposals, comments_dataframe=df_comments)
     init_csv(hsh_prop)
     init_txt(hsh_prop, sorting_attribute=sorting_attribute)
